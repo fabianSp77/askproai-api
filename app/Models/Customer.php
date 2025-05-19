70515965653e41de63c387da0b931c5fa5091131
fabianSp77 <fabianspitzer@icloud.com>
2025-05-19 12:23:52 +0200

    doc: annotate Customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,12 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Represents a business customer within the application.
+ *
+ * @property-read \Illuminate\Support\Collection|Branch[] $branches
+ */
 class Customer extends Model
 {
     use HasFactory;
 
+    /**
+     * Mass assignable attributes for the customer.
+     *
+     * @var array<string>
+     */
     protected $fillable = ['name', 'email', 'phone', 'notes'];
 
+    /**
+     * Branches that belong to the customer.
+     *
+     * @return HasMany
+     */
     public function branches(): HasMany
     {
         return $this->hasMany(Branch::class);
