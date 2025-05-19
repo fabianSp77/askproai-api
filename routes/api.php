ce467f65ed35e4360bb39f03821a5ab75361ba4a
fabianSp77 <fabianspitzer@icloud.com>
2025-05-19 12:41:45 +0200

    Improve comment for Retell webhook import

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalcomWebhookController;
-use App\Http\Controllers\RetellWebhookController;   // ← HIERHER!
+use App\Http\Controllers\RetellWebhookController;   // Handles webhook events from Retell
 
 /*
 |--------------------------------------------------------------------------
