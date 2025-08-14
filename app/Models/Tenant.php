1ecc175d1a08571f10f04aacffc4875d1508c168
root <root@v2202507255565358960.bestsrv.de>
2025-08-14 23:11:50 +0200

    fix(models): add HasFactory trait to Tenant model for tests

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Hash;
@@ -9,6 +10,7 @@ use Illuminate\Support\Str;
 
 class Tenant extends Model
 {
+    use HasFactory;
     public $incrementing = false;
 
     protected $keyType = 'string';
