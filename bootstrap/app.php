a33ca5c18ffb68c40d245254fb9605814ef2b6f4
SuperClaude <superclaude@askproai.de>
2025-10-08 16:47:18 +0200

    fix: restore 'view' container binding via ViewServiceProvider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health:   '/up',
     )
+    ->withProviders([
+        App\Providers\ViewBindingFixServiceProvider::class,
+    ])
     ->withMiddleware(function (Middleware $middleware) {
 
         /* ---------------------------------------------------------
